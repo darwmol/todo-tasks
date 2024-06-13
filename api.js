da3bec92ebfb6dd98ab6033050e1df193f5ffa6d
Darwin <darwmolina@gmail.com>
2024-06-13 00:30:04 -0500

    CRUD OK

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var express = require("express");
-var TaskModel = require('./task_schema');
+var TaskModel = require("./task_schema");
 var router = express.Router();
 
 let environment = null;
@@ -48,28 +48,59 @@ mongoose.connect(
   }
 );
 
-router.post('/create-task', function (req, res) {
-    let task_id = req.body.TaskId;
-    let name = req.body.Name;
-    let deadline = req.body.Deadline;
+router.post("/create-task", function (req, res) {
+  let task_id = req.body.TaskId;
+  let name = req.body.Name;
+  let deadline = req.body.Deadline;
 
-    let task = {
-        TaskId: task_id,
-        Name: name,
-        Deadline: deadline
+  let task = {
+    TaskId: task_id,
+    Name: name,
+    Deadline: deadline,
+  };
+  var newTask = new TaskModel(task);
+
+  newTask.save(function (err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal error\n");
+    } else {
+      res.status(200).send("OK\n");
     }
-    var newTask = new TaskModel(task);
+  });
+});
 
-    newTask.save(function (err, data) {
+router.get("/all-tasks", function (req, res) {
+  TaskModel.find(function (err, data) {
+    if (err) {
+      res.status(500).send("Internal error\n");
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+        Name: req.body.Name,
+        Deadline: req.body.Deadline
+    }, function (err, data) {
         if (err) {
-            console.log(err);
             res.status(500).send("Internal error\n");
-        }
-        else {
+        } else {
             res.status(200).send("OK\n");
         }
     });
 });
 
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
 
 module.exports = router;
